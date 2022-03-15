80c6490ac535491fcec19b670474425a84995235
Andreia-Santos5 <94610074+Andreia-Santos5@users.noreply.github.com>
2022-03-15 22:50:31 +0000

    Adicionado esqueleto para a página de sign in

---
+++ src/components/SignInForm.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div class="login-wrap">
+      <div class="login-html">
+        <input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Sign In</label>          
+        <input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>
+        <div class="login-form">
+        <!-- Acrescentei o form; bem como o "required", e o minlenght de letras para password e username-->
+          <form class="cmxform" id="signinForm" method="get" action="About.html">  
+            <div class="sign-in-htm">
+              <div class="group">
+                <label for="cname" class="label">Username</label>
+                <input id="cname" name="name" minlength="6" type="text" class="input" required>
+              </div>
+              <div class="group">
+                <label for="pass" class="label">Password</label>
+                <input id="pass" type="password" name="passaworde" minlength="6" class="input" data-type="password" required>
+              </div>
+              <div class="group">
+                <input id="check" type="checkbox" class="check" checked>
+                <label for="check"><span class="icon"></span> Keep me Signed in</label>
+              </div>
+              <div class="group">
+                <input type="submit" class="button" value="Sign In">
+              </div>
+              <div class="hr"></div>
+              <div class="foot-lnk">
+                <a href="#forgot">Forgot Password?</a>
+              </div>
+            </div>
+          </form>
+        </div> 
+      </div> 
+    </div>    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignInForm',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/SigninView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="signin">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <SignInForm msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import SignInForm from '@/components/SignInForm.vue'
+
+export default {
+  name: 'SigninView',
+  components: {
+    SignInForm
+  }
+}
+</script>
